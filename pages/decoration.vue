2d2a320b7a5b300116b02833de7bab1db2b16a8d
shubhank <aminshubhank@gmail.com>
2025-02-13 11:24:27 +0530

    decoration complete

--- pages/decoration.vue
+++ pages/decoration.vue
@@ -5,7 +5,7 @@
       <v-row justify="center">
         <v-col cols="12" sm="8" md="6" lg="4" class="d-flex justify-center">
           <v-card
-            class="text-center card pa-4"
+            class="text-center card-1 pa-4"
             :class="{ 'slide-up-leave': isHiding }"
             elevation="2"
           >
@@ -57,9 +57,23 @@
 
     <!-- FOURTH DIV (Background Image) -->
     <div v-if="decoDiv" class="deco">
-      <div id="balloon-container"></div>
-      <v-row>
-        <v-col class="pt-14 d-flex flex-column align-center">
+      <!-- Balloon row 1 -->
+      <v-row
+        class="justify-space-between px-16 mx-10"
+        :class="{ 'fade-in': showBalloons, hidden: !showBalloons }"
+      >
+        <v-col cols="3">
+          <v-img src="/images/balloon-1.png"></v-img>
+        </v-col>
+        <v-col cols="3">
+          <v-img src="/images/birthday-text.png"></v-img>
+        </v-col>
+        <v-col cols="3">
+          <v-img src="/images/balloon-1.png"></v-img>
+        </v-col>
+      </v-row>
+      <v-row v-if="!showBalloons">
+        <v-col class="pt-10 d-flex flex-column align-center">
           <p class="deco-text">Need Some Balloons ??</p>
           <div>
             <button class="cta" @click="addBalloons">
@@ -72,6 +86,68 @@
           </div>
         </v-col>
       </v-row>
+
+      <v-row v-if="showAudio">
+        <v-col class="d-flex justify-center">
+          <div class="d-flex flex-column align-center">
+            <p class="deco-text">Play Music</p>
+            <div class="volume" @click="playMusic" :class="'fade-in'">
+              <input type="checkbox" class="volume-input" />
+              <div class="volume-icon">
+                <svg
+                  viewBox="0 0 24 24"
+                  width="24"
+                  height="24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="volume-svg"
+                >
+                  <polygon points="11 5 6 9 2 9 2 15 6 15 11 19 11 5"></polygon>
+                  <path
+                    d="M19.07 4.93a10 10 0 0 1 0 14.14M15.54 8.46a5 5 0 0 1 0 7.07"
+                  ></path>
+                </svg>
+              </div>
+            </div>
+          </div>
+        </v-col>
+      </v-row>
+
+      <v-row v-if="showPics">
+        <v-col
+          v-for="(pic, index) in pics"
+          :key="index"
+          :class="[
+            'd-flex justify-center',
+            { 'fade-in': showPics && showPics[index] },
+            { 'mt-16 pt-10': index === 1 || index === pics.length - 1 }, // Apply mt-16 and pt-10 to the second and last cards
+          ]"
+        >
+          <div class="card">
+            <v-img :src="pic.src" cover></v-img>
+            <div class="card__content">
+              <p class="card__title">{{ pic.title }}</p>
+              <p class="card__description">{{ pic.description }}</p>
+            </div>
+          </div>
+        </v-col>
+      </v-row>
+      <!-- Balloon row 2 -->
+
+      <v-row
+        class="justify-space-between px-16 mx-10"
+        :class="{ 'fade-in-new': showBalloons, hidden: !showBalloons }"
+      >
+        <v-col cols="3">
+          <v-img src="/images/balloon-1.png"></v-img>
+        </v-col>
+        <v-col cols="3">
+          <v-img src="/images/balloon-1.png"></v-img>
+        </v-col>
+      </v-row>
     </div>
   </div>
 </template>
@@ -79,25 +155,48 @@
 <script setup>
 import { ref, onMounted } from "vue";
 
-const isVisible = ref(true); // Controls first div visibility
+// States
+const isVisible = ref(true);
 const isHiding = ref(false);
-const showLightsSection = ref(false); // Controls second div visibility
-const showDecorations = ref(false); // Controls third div visibility
+const showLightsSection = ref(false);
+const showDecorations = ref(false);
 const isDarkMode = ref(false);
 const decoDiv = ref(false);
+const showBalloons = ref(false);
+const showAudio = ref(false);
+let audio; // Declare audio outside of refs
+const showPics = ref(false);
+
 onMounted(() => {
+  // Initialize audio on mounted lifecycle hook
+  audio = new Audio("/audio/birthday.mp3");
+
   setTimeout(() => {
     isHiding.value = true;
     setTimeout(() => {
       isVisible.value = false;
       showLightsSection.value = true; // Show second div after first disappears
-    }, 1000); // Matches animation duration
+    }, 1000);
   }, 2000); // Wait 2 seconds before hiding first div
 });
+
+// Play music function
+const playMusic = () => {
+  audio.play();
+  showAudio.value = false;
+  setTimeout(() => {
+    showPics.value = true; // Start showing the pictures after some time
+  }, 1000);
+};
 
 const addDeco = () => {
   decoDiv.value = true;
   showDecorations.value = false;
+};
+
+const addBalloons = () => {
+  showBalloons.value = true;
+  showAudio.value = true; // Show balloons when button is clicked
 };
 
 const toggleMode = () => {
@@ -106,17 +205,46 @@ const toggleMode = () => {
     ? "#212121"
     : "#ffffff";
 
-  // Show third div when switch is toggled
   setTimeout(() => {
     showDecorations.value = true;
     showLightsSection.value = false;
   }, 300);
 };
 
+const pics = [
+  {
+    src: "/images/her1.jpg",
+    title: "Card Title 1",
+    description: "Lorem ipsum dolor sit amet.",
+  },
+  {
+    src: "/images/her2.jpg",
+    title: "Card Title 2",
+    description: "Lorem ipsum dolor sit amet.",
+  },
+  {
+    src: "/images/her3.jpeg",
+    title: "Card Title 3",
+    description: "Lorem ipsum dolor sit amet.",
+  },
+  {
+    src: "/images/her1.jpg",
+    title: "Card Title 4",
+    description: "Lorem ipsum dolor sit amet.",
+  },
+];
 </script>
 
 <style scoped>
-.card {
+:root {
+  --gps-burg: rgba(182, 15, 97, 0.9);
+  --gps-orange: rgba(242, 112, 45, 0.9);
+  --gps-skyblue: rgba(45, 181, 167, 0.9);
+  --gps-purple: rgba(190, 61, 244, 0.9);
+  --gps-green: rgba(180, 224, 67, 0.9);
+  --gps-yellow: rgba(242, 194, 58, 0.9);
+}
+.card-1 {
   position: absolute;
   top: 50%;
   width: 50vw;
@@ -164,6 +292,18 @@ const toggleMode = () => {
   top: 0;
   left: 0;
   transition: background-color 1s ease-in-out; /* Smooth transition */
+}
+
+.fade-in {
+  opacity: 1;
+  transform: scale(1);
+  transition: opacity 0.7s ease-in-out, transform 0.7s ease-in-out;
+}
+
+/* Initially hidden state for balloons */
+.hidden {
+  opacity: 0;
+  transform: scale(0.8);
 }
 
 /* Text Above the Button */
@@ -595,6 +735,276 @@ const toggleMode = () => {
   to {
     transform: translateY(-300vh);
     opacity: 0;
+  }
+}
+
+.box-canvas {
+  position: relative;
+  margin: auto;
+  display: block;
+  margin-bottom: 8%;
+  width: 230px;
+  height: 600px;
+  animation: floatUp 5s infinite linear;
+}
+.box-canvas-2 {
+  position: relative;
+  margin: auto;
+  display: block;
+  margin-bottom: 8%;
+  width: 230px;
+  height: 600px;
+  animation: floatUp 10s infinite linear;
+}
+
+.box-canvas-3 {
+  position: relative;
+  margin: auto;
+  display: block;
+  margin-bottom: 8%;
+  width: 230px;
+  height: 600px;
+  margin-left: 60px;
+  animation: floatUp 5s infinite linear;
+}
+
+.box-canvas-4 {
+  position: relative;
+  margin: auto;
+  display: block;
+  margin-bottom: 8%;
+  width: 230px;
+  height: 600px;
+  margin-left: 70px;
+  animation: floatUp 5s infinite linear;
+}
+
+@keyframes floatUp {
+  0% {
+    transform: translateY(100vh);
+  }
+
+  100% {
+    transform: translateY(-480px);
+  }
+}
+
+.red {
+  --balloon-color: var(--red);
+  --highlight-color: #fc9999;
+  --top-initial: 40px;
+  --string-angle: -20deg;
+}
+
+.yellow {
+  --balloon-color: #f2f24b;
+  --highlight-color: #e5e570;
+  --top-initial: 80px;
+  --left-initial: 50px;
+  --string-angle: -8deg;
+}
+
+.green {
+  --balloon-color: var(--green);
+  --highlight-color: #bad6d3;
+  --top-initial: 0;
+  --left-initial: 80px;
+  --string-angle: 1deg;
+}
+
+.blue {
+  --balloon-color: dodgerblue;
+  --highlight-color: #6ab5fc;
+  --left-initial: 100px;
+  --top-initial: 110px;
+  --string-angle: 10deg;
+}
+
+.orange {
+  --balloon-color: orange;
+  --highlight-color: #f9b94a;
+  --left-initial: 140px;
+  --top-initial: 50px;
+  --string-angle: 18deg;
+}
+
+.balloon-wrapper {
+  position: absolute;
+  left: var(--left-initial);
+  top: var(--top-initial);
+  width: 85px;
+}
+
+.string {
+  position: absolute;
+  top: 110px;
+  left: 42px;
+  transform: rotate(var(--string-angle));
+  transform-origin: top left;
+  width: 2px;
+  height: 250px;
+  background: #50535e;
+}
+
+.balloon {
+  position: absolute;
+  width: 85px;
+  height: 100px;
+  background: var(--balloon-color);
+  border-radius: 50%;
+}
+
+.balloon::before {
+  content: "";
+  position: absolute;
+  right: 20px;
+  top: 15px;
+  width: 15px;
+  height: 35px;
+  box-shadow: 5px 0 0 var(--highlight-color);
+  border-radius: 50%;
+  transform: rotate(-30deg);
+}
+
+.balloon::after {
+  content: "";
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: -15px;
+  width: 25px;
+  height: 20px;
+  background: var(--balloon-color);
+  clip-path: polygon(20% 0%, 80% 0%, 100% 100%, 0% 100%);
+}
+
+/* From Uiverse.io by barisdogansutcu */
+.volume {
+  width: 60px;
+  height: 60px;
+  border-radius: 100%;
+  background-color: rgb(205, 191, 247);
+  overflow: hidden;
+  position: relative;
+}
+.volume-icon {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.5s ease;
+  overflow: hidden;
+  position: relative;
+}
+.volume-input {
+  position: absolute;
+  inset: 0;
+  opacity: 0;
+  cursor: pointer;
+  z-index: 999;
+}
+.volume-icon:before {
+  content: "";
+  display: flex;
+  width: 0px;
+  height: 0px;
+  border-radius: 100%;
+  transition: all 0.2s ease;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  background-color: rgb(32, 33, 36);
+}
+.volume-input:checked + .volume-icon:before {
+  width: calc(100% + 4px);
+  height: calc(100% + 4px);
+}
+.volume-svg {
+  z-index: 2;
+  color: rgb(32, 33, 36);
+  height: 30px;
+  width: 30px;
+}
+.volume-input:checked + .volume-icon svg {
+  color: rgb(255, 255, 255);
+}
+
+/* From Uiverse.io by gharsh11032000 */
+.card {
+  position: relative;
+  width: 300px;
+  height: 200px;
+  background-color: #f2f2f2;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  perspective: 1000px;
+  box-shadow: 0 0 0 5px #ffffff80;
+  transition: all 0.6s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  cursor: pointer;
+}
+
+.card svg {
+  width: 48px;
+  fill: #333;
+  transition: all 0.6s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+}
+
+.card:hover {
+  transform: scale(1.05);
+  box-shadow: 0 8px 16px rgba(255, 255, 255, 0.2);
+}
+
+.card__content {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  box-sizing: border-box;
+  background-color: #f2f2f2;
+  transform: rotateX(-90deg);
+  transform-origin: bottom;
+  transition: all 0.6s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+}
+
+.card:hover .card__content {
+  transform: rotateX(0deg);
+}
+
+.card__title {
+  margin: 0;
+  font-size: 24px;
+  color: #333;
+  font-weight: 700;
+}
+
+.card:hover svg {
+  scale: 0;
+}
+
+.card__description {
+  margin: 10px 0 0;
+  font-size: 14px;
+  color: #777;
+  line-height: 1.4;
+}
+
+.fade-in-new {
+  opacity: 0;
+  transform: translateY(20px);
+  animation: fadeInUp 0.9s ease-in-out;
+}
+
+@keyframes fadeInUp {
+  to {
+    opacity: 1;
+    transform: translateY(0);
   }
 }
 </style>
